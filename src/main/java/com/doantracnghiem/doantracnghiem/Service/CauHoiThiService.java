9899c4d0545d067adb0194e421f09a36da74f64b
Phuong Nguyen <hnguyenphuong09@gmail.com>
2024-02-13 23:00:33 +0700

    add luucauhoithi function to save data after taken exam

--- src/main/java/com/doantracnghiem/doantracnghiem/Service/CauHoiThiService.java
+++ src/main/java/com/doantracnghiem/doantracnghiem/Service/CauHoiThiService.java
@@ -6,10 +6,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.doantracnghiem.doantracnghiem.Data_Transfer_Object.CauHoiThiDTO;
+import com.doantracnghiem.doantracnghiem.Data_Transfer_Object.DanhSachCauHoiThiDTO;
+import com.doantracnghiem.doantracnghiem.Entity.CTBaiThi;
 import com.doantracnghiem.doantracnghiem.Entity.CauHoi;
 import com.doantracnghiem.doantracnghiem.Entity.LuaChon;
+import com.doantracnghiem.doantracnghiem.Repository.CTBaiThiRepository;
 import com.doantracnghiem.doantracnghiem.Repository.DanhSachCauHoiRepository;
 import com.doantracnghiem.doantracnghiem.Repository.LuaChonRepository;
+import com.doantracnghiem.doantracnghiem.Repository.ThiRepository;
 
 @Service
 public class CauHoiThiService {
@@ -17,17 +21,37 @@ public class CauHoiThiService {
     private DanhSachCauHoiRepository danhSachCauHoiRepository;
     @Autowired
     private LuaChonRepository myLuaChonRepository;
-    public List<CauHoiThiDTO> layCauHoiThi(String mamh,int socau){
-        List<CauHoiThiDTO> listCauHoiThi = new ArrayList<>();
+    @Autowired
+    private CTBaiThiRepository ctBaiThiRepository;
+    @Autowired
+    private ThiRepository thiRepository;
+
+    public DanhSachCauHoiThiDTO layCauHoiThi(String mamh, int socau, int idthi) {
+        DanhSachCauHoiThiDTO listCauHoiThi = new DanhSachCauHoiThiDTO();
         List<CauHoi> listCauHoi;
         List<LuaChon> listLuaChon;
+        CauHoiThiDTO tmp;
+        CTBaiThi tmp1;
+        int thuTuChon = 1;
         listCauHoi = danhSachCauHoiRepository.layCauHoiThi(mamh, socau);
-        CauHoiThiDTO tmp ;
-        for(CauHoi ch : listCauHoi){
+        System.out.println("tai service");
+        for (CauHoi ch : listCauHoi) {
             listLuaChon = myLuaChonRepository.findAllByIdch(ch.getIdch());
-            tmp = new CauHoiThiDTO(ch,listLuaChon);
-            listCauHoiThi.add(tmp);
+            tmp1 = new CTBaiThi(idthi);
+            tmp1.setIdch(ch.getIdch());
+            tmp1.setThuTuChon(thuTuChon++);
+            tmp = new CauHoiThiDTO(ch, listLuaChon, tmp1);
+            listCauHoiThi.addCauHoiThi(tmp);
         }
         return listCauHoiThi;
+    }
+    public void luuCauHoiThi(DanhSachCauHoiThiDTO list ){
+        System.out.println("save");
+        List<CTBaiThi> ctBaiThi = new ArrayList<>();
+        for(CauHoiThiDTO ch:list.getList()){
+            ctBaiThi.add(ch.getCtBaiThi());
+        }
+        thiRepository.update(ctBaiThi.get(0).getIdThi());
+        ctBaiThiRepository.saveAll(ctBaiThi);
     }
 }
