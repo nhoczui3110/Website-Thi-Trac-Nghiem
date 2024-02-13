7199f7ecccb920a774d66cc41913ca53593d6418
Phuong Nguyen <hnguyenphuong09@gmail.com>
2024-02-13 22:54:44 +0700

    add idthi property and its function

--- src/main/java/com/doantracnghiem/doantracnghiem/Data_Transfer_Object/LichThi.java
+++ src/main/java/com/doantracnghiem/doantracnghiem/Data_Transfer_Object/LichThi.java
@@ -3,9 +3,17 @@ package com.doantracnghiem.doantracnghiem.Data_Transfer_Object;
 import java.io.Serializable;
 import java.sql.Date;
 
-
 public class LichThi implements Serializable{
-    public LichThi(String maMh ,String tenMH, Date ngayThi, Integer lanThi, Integer soCau, Integer thoiLuong, Boolean trangThai) {
+    private int IDTHI;
+    private String maMH;
+    private Integer lanThi;
+    private Integer soCau;
+    private Integer thoiLuong;
+    private Boolean trangThai;
+	private String tenMH;
+    private Date ngayThi;
+    public LichThi(int IDTHI,String maMh ,String tenMH, Date ngayThi, Integer lanThi, Integer soCau, Integer thoiLuong, Boolean trangThai) {
+        this.IDTHI = IDTHI;
         this.maMH = maMh;
         this.tenMH = tenMH;
         this.ngayThi = ngayThi;
@@ -14,15 +22,18 @@ public class LichThi implements Serializable{
         this.thoiLuong = thoiLuong;
         this.trangThai = trangThai;
     }
-    private String maMH;
+    public int getIDTHI() {
+        return IDTHI;
+    }
+    public void setIDTHI(int iDTHI) {
+        IDTHI = iDTHI;
+    }
     public String getMaMH() {
         return maMH;
     }
     public void setMaMH(String maMH) {
         this.maMH = maMH;
     }
-    private String tenMH;
-    private Date ngayThi;
     public String getTenMH() {
         return tenMH;
     }
@@ -41,8 +52,4 @@ public class LichThi implements Serializable{
     public Boolean isTrangThai() {
         return trangThai;
     }
-    private Integer lanThi;
-    private Integer soCau;
-    private Integer thoiLuong;
-    private Boolean trangThai;
 }
