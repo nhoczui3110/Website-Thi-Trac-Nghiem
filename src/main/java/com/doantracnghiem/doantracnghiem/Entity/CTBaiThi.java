cfd78acce506c4192f4c95fb5c2fbcf06c372adb
Phuong Nguyen <hnguyenphuong09@gmail.com>
2024-02-13 22:55:33 +0700

    modify CTBaiThi

--- src/main/java/com/doantracnghiem/doantracnghiem/Entity/CTBaiThi.java
+++ src/main/java/com/doantracnghiem/doantracnghiem/Entity/CTBaiThi.java
@@ -13,10 +13,53 @@ public class CTBaiThi {
     @Id
     @Column(name = "IDTHI")
     private int idThi;
-    @Id
+    public CTBaiThi(int idThi) {
+		this.idThi = idThi;
+		dapAnSv = 0;
+	}
+	@Id
     @Column(name = "IDCH")
     private int idch;
-    @Column(name = "DAPANSV")
+    public CTBaiThi(int idThi, int idch, int dapAnSv, int thuTuChon, boolean trangThaiXoa) {
+		this.idThi = idThi;
+		this.idch = idch;
+		this.dapAnSv = dapAnSv;
+		this.thuTuChon = thuTuChon;
+		this.trangThaiXoa = trangThaiXoa;
+	}
+	public int getIdThi() {
+		return idThi;
+	}
+	public CTBaiThi() {
+	}
+	public void setIdThi(int idThi) {
+		this.idThi = idThi;
+	}
+	public int getIdch() {
+		return idch;
+	}
+	public void setIdch(int idch) {
+		this.idch = idch;
+	}
+	public int getDapAnSv() {
+		return dapAnSv;
+	}
+	public void setDapAnSv(int dapAnSv) {
+		this.dapAnSv = dapAnSv;
+	}
+	public int getThuTuChon() {
+		return thuTuChon;
+	}
+	public void setThuTuChon(int thuTuChon) {
+		this.thuTuChon = thuTuChon;
+	}
+	public boolean isTrangThaiXoa() {
+		return trangThaiXoa;
+	}
+	public void setTrangThaiXoa(boolean trangThaiXoa) {
+		this.trangThaiXoa = trangThaiXoa;
+	}
+	@Column(name = "DAPANSV")
     private int dapAnSv;
     @Column(name = "THUTUCHON")
     private int thuTuChon;
