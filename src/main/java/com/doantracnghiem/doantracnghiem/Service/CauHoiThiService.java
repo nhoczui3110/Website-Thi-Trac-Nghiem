9cecffa317fd420e1a95f5d7e7309b26536b2094
Phuong Nguyen <hnguyenphuong09@gmail.com>
2024-02-19 20:50:56 +0700

    create layKetQua and ChenDapAn function

--- src/main/java/com/doantracnghiem/doantracnghiem/Service/CauHoiThiService.java
+++ src/main/java/com/doantracnghiem/doantracnghiem/Service/CauHoiThiService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.doantracnghiem.doantracnghiem.Data_Transfer_Object.CauHoiThiDTO;
 import com.doantracnghiem.doantracnghiem.Data_Transfer_Object.DanhSachCauHoiThiDTO;
+import com.doantracnghiem.doantracnghiem.Data_Transfer_Object.KetQuaThiDTO;
 import com.doantracnghiem.doantracnghiem.Entity.CTBaiThi;
 import com.doantracnghiem.doantracnghiem.Entity.CauHoi;
 import com.doantracnghiem.doantracnghiem.Entity.LuaChon;
@@ -36,6 +37,7 @@ public class CauHoiThiService {
         listCauHoi = danhSachCauHoiRepository.layCauHoiThi(mamh, socau,dap_an);
         for (CauHoi ch : listCauHoi) {
             listLuaChon = myLuaChonRepository.findAllByIdch(ch.getIdch());
+            listLuaChon.sort((lc1,lc2)->(lc1.getThuTu() - lc2.getThuTu()));
             tmp1 = new CTBaiThi(idthi);
             tmp1.setIdch(ch.getIdch());
             tmp1.setThuTuChon(thuTuChon++);
@@ -44,22 +46,31 @@ public class CauHoiThiService {
         }
         return listCauHoiThi;
     }
-    public void luuCauHoiThi(DanhSachCauHoiThiDTO list, List<Integer> dap_an ){
+    public float luuCauHoiThi(DanhSachCauHoiThiDTO list ){
         float diem = 0;
-        System.out.println(dap_an);
-        int soCauDung = 0,tongSoCau = 0,index = 0;
+        int soCauDung = 0,tongSoCau = 0;
         List<CTBaiThi> ctBaiThi = new ArrayList<>();
         for(CauHoiThiDTO ch:list.getList()){
             ctBaiThi.add(ch.getCtBaiThi());
-            if(ch.getCtBaiThi().getDapAnSv() == dap_an.get(index++))soCauDung ++;
-            System.out.println(ch.getCtBaiThi().getDapAnSv());
+            if(ch.getCtBaiThi().getDapAnSv() == ch.getCauHoi().getDapAnDung())soCauDung ++;
+            System.out.println(ch.getCtBaiThi().getDapAnSv()+ "   |   " +ch.getCauHoi().getDapAnDung());
             tongSoCau ++;
         }
         System.out.println(soCauDung);
-        diem = (float)soCauDung/tongSoCau;
+        diem = (float)soCauDung/tongSoCau*10;
         diem = (float)Math.round(diem*100)/100;
         System.out.println(diem);
         thiRepository.update(ctBaiThi.get(0).getIdThi(),diem);
         ctBaiThiRepository.saveAll(ctBaiThi);
+        return diem;
+    }
+    public void chenDapAn(DanhSachCauHoiThiDTO danhSachCauHoiThiDTO, List<Integer> dap_an){
+        for (int i = 0; i<danhSachCauHoiThiDTO.getList().size();i++){
+            danhSachCauHoiThiDTO.getList().get(i).getCauHoi().setDapAnDung(dap_an.get(i));
+        }
+    }
+    public KetQuaThiDTO layKetQua(int idThi){
+        KetQuaThiDTO kq = ctBaiThiRepository.layThongTinKQ(idThi);
+        return kq;
     }
 }
