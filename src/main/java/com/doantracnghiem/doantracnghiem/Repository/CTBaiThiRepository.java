c330bf25466a8c108918a1aa9743476c7be7048b
Phuong Nguyen <hnguyenphuong09@gmail.com>
2024-02-19 20:47:53 +0700

    add KetQuaThiDTO function

--- src/main/java/com/doantracnghiem/doantracnghiem/Repository/CTBaiThiRepository.java
+++ src/main/java/com/doantracnghiem/doantracnghiem/Repository/CTBaiThiRepository.java
@@ -1,12 +1,16 @@
 package com.doantracnghiem.doantracnghiem.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.doantracnghiem.doantracnghiem.Data_Transfer_Object.KetQuaThiDTO;
 import com.doantracnghiem.doantracnghiem.Entity.CTBaiThi;
 import com.doantracnghiem.doantracnghiem.Entity.IDCTBaiThi;
 
 @Repository
 public interface CTBaiThiRepository extends JpaRepository<CTBaiThi,IDCTBaiThi>{
-    
+    @Query(value =  "Exec layThongTinKQ @idThi= :idThi",nativeQuery = true)
+    public KetQuaThiDTO layThongTinKQ(@Param("idThi") int idThi);
 }
