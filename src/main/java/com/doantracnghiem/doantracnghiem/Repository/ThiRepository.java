458300795bdf605ad73c16d7fdbd9568429ee317
Phuong Nguyen <hnguyenphuong09@gmail.com>
2024-02-27 21:35:53 +0700

    add checkExsistThi function to check whether exsist or not row thi in database

--- src/main/java/com/doantracnghiem/doantracnghiem/Repository/ThiRepository.java
+++ src/main/java/com/doantracnghiem/doantracnghiem/Repository/ThiRepository.java
@@ -8,13 +8,17 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.doantracnghiem.doantracnghiem.DTO.ThiInfoDTO;
-import com.doantracnghiem.doantracnghiem.Entity.LuaChon;
+// import com.doantracnghiem.doantracnghiem.Entity.LuaChon;
+import com.doantracnghiem.doantracnghiem.Entity.Thi;
 
 @Repository
-public interface ThiRepository extends JpaRepository<LuaChon, Integer> {
+public interface ThiRepository extends JpaRepository<Thi, Integer> {
     @Query(value = "EXEC findThiFromIddk @iddk = :iddk", nativeQuery = true)
     public List<ThiInfoDTO> findThiInfoFromIddk(@Param("iddk") int iddk);
 
     @Query(value = "EXEC insertThiByIddk @iddk = :iddk", nativeQuery = true)
     public void insertThiByIddk(@Param("iddk") int iddk);
+
+    @Query(value =  "SELECT * FROM Thi t where t.masv = :masv and t.trangThaiXoa = 0",nativeQuery=true)
+    public List<Thi> checkExsitsThi(@Param("masv") String masv);
 }
