598d5247d0d3953711bfc78d31152863f7a960af
Phuong Nguyen <hnguyenphuong09@gmail.com>
2024-03-04 19:31:08 +0700

    change KetquaDTO to map

--- src/main/java/com/doantracnghiem/doantracnghiem/Repository/CTBaiThiRepository.java
+++ src/main/java/com/doantracnghiem/doantracnghiem/Repository/CTBaiThiRepository.java
@@ -1,16 +1,17 @@
 package com.doantracnghiem.doantracnghiem.Repository;
 
+import java.util.Map;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.doantracnghiem.doantracnghiem.Data_Transfer_Object.KetQuaThiDTO;
 import com.doantracnghiem.doantracnghiem.Entity.CTBaiThi;
 import com.doantracnghiem.doantracnghiem.Entity.IDCTBaiThi;
 
 @Repository
 public interface CTBaiThiRepository extends JpaRepository<CTBaiThi,IDCTBaiThi>{
     @Query(value =  "Exec layThongTinKQ @idThi= :idThi",nativeQuery = true)
-    public KetQuaThiDTO layThongTinKQ(@Param("idThi") int idThi);
+    public Map<String,Object> layThongTinKQ(@Param("idThi") int idThi);
 }
