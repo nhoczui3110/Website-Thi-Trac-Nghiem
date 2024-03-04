6b17a5e1b9560a7339c16c7bb95c5f9dc2ac3f51
Phuong Nguyen <hnguyenphuong09@gmail.com>
2024-03-04 19:28:35 +0700

    add function getSinhVien to send student info to view and shorten all function

--- src/main/java/com/doantracnghiem/doantracnghiem/Controller/ThiController.java
+++ src/main/java/com/doantracnghiem/doantracnghiem/Controller/ThiController.java
@@ -7,10 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+
 import com.doantracnghiem.doantracnghiem.Data_Transfer_Object.DanhSachCauHoiThiDTO;
-import com.doantracnghiem.doantracnghiem.Data_Transfer_Object.Diem;
-import com.doantracnghiem.doantracnghiem.Data_Transfer_Object.KetQuaThiDTO;
-import com.doantracnghiem.doantracnghiem.Data_Transfer_Object.LichThi;
+import com.doantracnghiem.doantracnghiem.Data_Transfer_Object.InfoDTO;
 import com.doantracnghiem.doantracnghiem.Entity.SinhVien;
 import com.doantracnghiem.doantracnghiem.Service.CauHoiThiService;
 import com.doantracnghiem.doantracnghiem.Service.KetQuaThiService;
@@ -36,17 +37,18 @@ public class ThiController {
     private SinhVienService sinhVienService;
     @Autowired 
     private KetQuaThiService ketQuaThiService;
-    // @ModelAttribute("ketqua")
+    @ModelAttribute("sinhvien")
+    public SinhVien getSinhVien(HttpSession session){
+        session.setAttribute("masv", "N21DCCN067");//
+        String sv = (String)session.getAttribute("masv");
+        return sinhVienService.getSinhVienInfo(sv);
+    }
     @GetMapping("/main-student")
     public String main_student(Model model, HttpSession session) {
-        System.out.println('s');
-        session.setAttribute("masv", "N21DCCN067");
+        session.setAttribute("masv", "N21DCCN067");//
         String masv = (String)session.getAttribute("masv");
-        List<LichThi> lichthi = lichThiService.xemLichThi(masv);
-        List<Diem> diem = xemDiemThiService.xemDiemThi(masv);
-        SinhVien sv = sinhVienService.getSinhVienInfo(masv);
-        model.addAttribute("sv", sv);
-        // model.addAttribute("masv", masv);
+        List<InfoDTO> lichthi = lichThiService.xemLichThi(masv);
+        List<InfoDTO> diem = xemDiemThiService.xemDiemThi(masv);
         model.addAttribute("lichthi", lichthi);
         model.addAttribute("diemthi", diem);
         return "/student/main-student";
@@ -61,67 +63,30 @@ public class ThiController {
     @RequestParam(name = "thoiluong") Integer thoiLuong,
     HttpSession session,
     Model model){
-        System.out.println("====================");
-        System.out.println(maMH);
-        System.out.println(IDTHI);
-        System.out.println("====================");
         List<Integer> dap_an = new ArrayList<>();
         DanhSachCauHoiThiDTO list = cauHoiThiService.layCauHoiThi(maMH, soCau,IDTHI,dap_an);
-        model.addAttribute("idthi", IDTHI);
-        model.addAttribute("tenmh",tenMh);
-        model.addAttribute("ngaythi",ngayThi);
-        model.addAttribute("lanthi",lanThi);
-        model.addAttribute("socau",soCau);
-        model.addAttribute("thoiluong",thoiLuong);
+        InfoDTO thongTinThi = new InfoDTO(IDTHI, lanThi, soCau, thoiLuong, tenMh, ngayThi);
+        model.addAttribute("thongTinThi", thongTinThi);
         model.addAttribute("danhsachch", list);
-        session.setAttribute("tenmh",tenMh);
-        session.setAttribute("ngaythi",ngayThi);
-        session.setAttribute("lanthi",lanThi);
-        session.setAttribute("socau",soCau);
-        session.setAttribute("thoiluong",thoiLuong);
-        session.setAttribute("danhsachch", list);
-        session.setAttribute("dapan", dap_an);
+        session.setAttribute("dapan"+IDTHI, dap_an);
         return "/student/thi";
     }
     @PostMapping("/ketqua")
     @SuppressWarnings("unchecked")
-    public String tmp(@ModelAttribute DanhSachCauHoiThiDTO danhSach,HttpSession session,Model model){
-        List<Integer> dap_an = (List<Integer>)session.getAttribute("dapan");
+    public RedirectView tmp(@ModelAttribute DanhSachCauHoiThiDTO danhSach,HttpSession session,RedirectAttributes redirect){
+        int idThi = danhSach.getIdThi();
+        List<Integer> dap_an = (List<Integer>)session.getAttribute("dapan" + idThi);
+        session.removeAttribute("dapan"+idThi);
         cauHoiThiService.chenDapAn(danhSach, dap_an);
-        float diem = cauHoiThiService.luuCauHoiThi(danhSach);
-        String tenMh = (String)session.getAttribute("tenmh");
-        Date ngayThi = (Date) session.getAttribute("ngaythi");
-        int lanThi = (int) session.getAttribute("lanthi");
-        int soCau = (int) session.getAttribute("socau");
-        int thoiLuong = (int) session.getAttribute("thoiluong");
-        
-        session.removeAttribute("dapan");
-        session.removeAttribute("tenmh");
-        session.removeAttribute("ngaythi");
-        session.removeAttribute("lanthi");
-        session.removeAttribute("socau");
-        session.removeAttribute("thoiluong");
-
-
-        model.addAttribute("tenmh",tenMh);
-        model.addAttribute("ngaythi",ngayThi);
-        model.addAttribute("lanthi",lanThi);
-        model.addAttribute("socau",soCau);
-        model.addAttribute("thoiluong",thoiLuong);
-        model.addAttribute("danhsach", danhSach);
-        model.addAttribute("diem", diem);
-        return "/student/exam-result";
+        cauHoiThiService.luuCauHoiThi(danhSach);
+        redirect.addAttribute("idThi",idThi);
+        return new RedirectView("xemketqua");
     }
     @GetMapping("/xemketqua")
     public String xemKetQua(@RequestParam(name = "idThi") int idThi,Model model){
-        KetQuaThiDTO kq = cauHoiThiService.layKetQua(idThi);
-        model.addAttribute("tenmh",kq.getTenMh());
-        model.addAttribute("ngaythi",kq.getNgayThi());
-        model.addAttribute("lanthi",kq.getLan());
-        model.addAttribute("socau",kq.getSoCau());
-        model.addAttribute("thoiluong",kq.getThoiLuong());
-        model.addAttribute("diem", kq.getDiem());
+        InfoDTO kq = ketQuaThiService.layKetQua(idThi);
         DanhSachCauHoiThiDTO danhSach = ketQuaThiService.layDanhSachCauHoiThi(idThi);
+        model.addAttribute("thongTinKq", kq);
         model.addAttribute("danhsach", danhSach);
         return "/student/exam-result"; 
     }
