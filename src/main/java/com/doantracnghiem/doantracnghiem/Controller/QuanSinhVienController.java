0866e48b350a003f06769d52649ef73fb94fce3d
Phuong Nguyen <hnguyenphuong09@gmail.com>
2024-03-17 22:49:05 +0700

    add getStudenByClass and searchStudentByKeyword function

--- src/main/java/com/doantracnghiem/doantracnghiem/Controller/QuanSinhVienController.java
+++ src/main/java/com/doantracnghiem/doantracnghiem/Controller/QuanSinhVienController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.doantracnghiem.doantracnghiem.Entity.SinhVien;
@@ -24,22 +25,27 @@ public class QuanSinhVienController {
     @Autowired private QuanLySinhVienService quanLySinhVienService;
     @GetMapping("/student")
     public List<SinhVien> getAllStudent(){
-        System.out.println(quanLySinhVienService.getAllStudent());
         return quanLySinhVienService.getAllStudent();
     }
     @GetMapping("/student/{masv}")
     public SinhVien getStudent(@PathVariable(name = "masv") String masv) {
         return quanLySinhVienService.getStudent(masv);
     }
+    @GetMapping("/studentbyclass/{malop}")
+    public List<SinhVien> getStudentByClass(@PathVariable(name = "malop") String malop){
+        return quanLySinhVienService.getStudentByClass(malop);
+    }
+    @GetMapping("/student/search")
+    public List<SinhVien> searchStudetnsByKeyword(@RequestParam("keyword") String keyword,@RequestParam("malop")String maLop){
+        return quanLySinhVienService.searchStudetnsByKeyword(keyword,maLop);
+    }
     @DeleteMapping("/student/{masv}")
     public ResponseEntity<String> deleteStudent(@PathVariable(name = "masv") String masv){
-        System.out.println("==============loi==================");
         return quanLySinhVienService.deleteStudent(masv);
     }
     //sinh viên cập nhật thông tin
     @PatchMapping("/student")
     public int updateInfo(@RequestBody Map<String,Object> studentInfo){
-        System.out.println(studentInfo.toString());
         try{
             quanLySinhVienService.updateInfo(studentInfo);
             return 1;
@@ -52,13 +58,11 @@ public class QuanSinhVienController {
     // nhan vien sửa thông tin
     @PatchMapping("/mstudent/{masv}")
     public ResponseEntity<String> modifyInfo(@PathVariable(name = "masv") String masv, @RequestBody Map<String,Object> studentInfo){
-        System.out.println(studentInfo.toString());
             quanLySinhVienService.modifyInfo(studentInfo);
             return ResponseEntity.ok().body("Sửa thông tin thành công");
     }
     @PostMapping("/student")
     public ResponseEntity<String> addStudent(@RequestBody SinhVien sv){
-        System.out.println("den day");
         return quanLySinhVienService.addStudent(sv);
     }
 }
