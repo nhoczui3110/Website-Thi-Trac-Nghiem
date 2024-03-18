843715514d84f34bdfb0ef05ccb88c0007c10e01
Phuong Nguyen <hnguyenphuong09@gmail.com>
2024-03-18 22:45:06 +0700

    using hibernate instead JDBC

--- src/main/java/com/doantracnghiem/doantracnghiem/Repository/DiemRepository.java
+++ src/main/java/com/doantracnghiem/doantracnghiem/Repository/DiemRepository.java
@@ -6,6 +6,12 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.sound.midi.MidiDevice.Info;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.doantracnghiem.doantracnghiem.ConnectDataObject.JDBCUtil;
@@ -13,34 +19,17 @@ import com.doantracnghiem.doantracnghiem.Data_Transfer_Object.InfoDTO;
 
 @Repository
 public class DiemRepository {
+    @Autowired
+    private SessionFactory sessionFactory;
     public List<InfoDTO> layDanhSachDiem(String masv){
-        List<InfoDTO> diems = new ArrayList<>();
-        Connection c = JDBCUtil.getConnection();
-        String spCall = "{call xemDiemThi(?)}";
-        try (CallableStatement cs = c.prepareCall(spCall)) {
-            cs.setString(1, masv);
-            ResultSet rs = cs.executeQuery();
-            // Lấy và hiển thị dữ liệu từ ResultSet
-            InfoDTO tmp = null;
-            String tenMH;
-            int lanThi;
-            float diem;
-            int idThi;
-            while (rs.next()) {
-                tenMH = rs.getString("TENMH");
-                lanThi = rs.getInt("LANTHI");
-                diem = rs.getFloat("DIEM");
-                idThi = rs.getInt("IDTHI");
-                tmp = new InfoDTO(idThi, lanThi, tenMH, diem);
-                diems.add(tmp);
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        finally{
-            JDBCUtil.closeConnection(c);
-        }
+        Session session = sessionFactory.openSession();
+        List<InfoDTO> diems = null;
+        String spCall = "exec xemDiemThi " + masv;
+        session.beginTransaction();
+        Query<InfoDTO> query = session.createNativeQuery(spCall, InfoDTO.class);
+        diems =  query.getResultList();
+        session.getTransaction().commit();
+        session.close();
         return diems;
     }
 }
