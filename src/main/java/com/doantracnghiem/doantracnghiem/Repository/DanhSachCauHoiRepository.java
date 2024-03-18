eb322d60543544d6aeb286862b52ea7857a52afe
Phuong Nguyen <hnguyenphuong09@gmail.com>
2024-03-18 22:44:21 +0700

    using hibernate instead JDBC

--- src/main/java/com/doantracnghiem/doantracnghiem/Repository/DanhSachCauHoiRepository.java
+++ src/main/java/com/doantracnghiem/doantracnghiem/Repository/DanhSachCauHoiRepository.java
@@ -3,14 +3,17 @@ package com.doantracnghiem.doantracnghiem.Repository;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import com.doantracnghiem.doantracnghiem.ConnectDataObject.JDBCUtil;
 import com.doantracnghiem.doantracnghiem.Data_Transfer_Object.CauHoiThiDTO;
 import com.doantracnghiem.doantracnghiem.Data_Transfer_Object.DanhSachCauHoiThiDTO;
+import com.doantracnghiem.doantracnghiem.Data_Transfer_Object.ResultSetDTO;
 import com.doantracnghiem.doantracnghiem.Entity.CTBaiThi;
 import com.doantracnghiem.doantracnghiem.Entity.CauHoi;
 import com.doantracnghiem.doantracnghiem.Entity.LuaChon;
@@ -19,82 +22,44 @@ import com.doantracnghiem.doantracnghiem.Entity.LuaChon;
 public class DanhSachCauHoiRepository {
     @Autowired
     private LuaChonRepository luaChonRepository;
+    @Autowired 
+    private SessionFactory sessionFactory;
     public List<CauHoi> layCauHoiThi(String maMH,int soCau,List<Integer> dap_an){
-        List<CauHoi> list = new ArrayList<>();
-        Connection c = JDBCUtil.getConnection();
-        String spCall = "{call layDanhSachCauHoi(?,?)}";
-        try (CallableStatement cs = c.prepareCall(spCall)) {
-            cs.setString(1, maMH);
-            cs.setInt(2, soCau);
-            ResultSet rs = cs.executeQuery();
-            // Lấy và hiển thị dữ liệu từ ResultSet
-            CauHoi tmp = null;
-            int IDCH;
-            String hinhThuc;
-            String noiDung;
-            int iddh;
-            int dapandung;
-            while (rs.next()) {
-                IDCH = rs.getInt("IDCH");
-                hinhThuc = rs.getString("HINHTHUC");
-                noiDung = rs.getString("NOIDUNG");
-                iddh = rs.getInt("IDDH");
-                dapandung = rs.getInt("DAPANDUNG");
-                dap_an.add(dapandung);
-                tmp = new CauHoi(IDCH,hinhThuc,noiDung,0,iddh,false);
-                list.add(tmp);
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
+        List<CauHoi> list =null;
+        String spCall = "exec layDanhSachCauHoi '" + maMH + "', "+ soCau ;
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        Query<CauHoi> query = session.createNativeQuery(spCall, CauHoi.class);
+        list=query.getResultList();
+        session.getTransaction().commit();
+        for(CauHoi c:list ){
+            dap_an.add(c.getDapAnDung());
+            c.setDapAnDung(0);
         }
-        finally{
-            JDBCUtil.closeConnection(c);
-        }
+        session.close();
         return list;
     }
     public DanhSachCauHoiThiDTO layCauHoiDaThi(int idThi){
         DanhSachCauHoiThiDTO ds = new DanhSachCauHoiThiDTO();
-        // List<CauHoi> list = new ArrayList<>();
-        Connection c = JDBCUtil.getConnection();
-        String spCall = "{call layCauHoiDaThi(?)}";
-        try (CallableStatement cs = c.prepareCall(spCall)) {
-            cs.setInt(1, idThi);
-            ResultSet rs = cs.executeQuery();
-            // Lấy và hiển thị dữ liệu từ ResultSet
-            CauHoi tmp = null;
-            List<LuaChon> tmp1 = null;
-            CTBaiThi tmp2 = null;
-            int IDCH;
-            String hinhThuc;
-            String noiDung;
-            int iddh;
-            int dapAnDung;
-            int dapAnSv;
-            int thuTuChon;
-            while (rs.next()) {
-                IDCH = rs.getInt("IDCH");
-                hinhThuc = rs.getString("HINHTHUC");
-                noiDung = rs.getString("NOIDUNG");
-                iddh = rs.getInt("IDDH");
-                dapAnDung = rs.getInt("DAPANDUNG");
-                dapAnSv = rs.getInt("DAPANSV");
-                thuTuChon = rs.getInt("THUTUCHON");
-
-                tmp = new CauHoi(IDCH,hinhThuc,noiDung,dapAnDung,iddh,false);
-                tmp2 = new CTBaiThi(idThi, IDCH, dapAnSv, thuTuChon, false);
-                tmp1 = luaChonRepository.findAllByIdch(IDCH);
-                tmp1.sort((lc1,lc2)->(lc1.getThuTu() - lc2.getThuTu()));
-                ds.addCauHoiThi(new CauHoiThiDTO(tmp, tmp1, tmp2));
-            }
-            ds.getList().sort((ds1,ds2)->(ds1.getCtBaiThi().getThuTuChon() - ds2.getCtBaiThi().getThuTuChon()));
-            
-        } catch (Exception e) {
-            e.printStackTrace();
+        List<ResultSetDTO> rs ;
+        CauHoi cauHoi = null;
+        CTBaiThi ctBaiThi = null;
+        List<LuaChon> luaChons = null;
+        String spCall = "exec layCauHoiDaThi " + idThi;
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        Query<ResultSetDTO> query = session.createNativeQuery(spCall, ResultSetDTO.class);
+        rs = query.getResultList();
+        session.getTransaction().commit();
+        for(ResultSetDTO r: rs){
+            cauHoi = new CauHoi(r.getIdch(),r.getHinhThuc(),r.getNoiDung(),r.getDapAnDung(),r.getIddh(),false);
+            ctBaiThi = new CTBaiThi(idThi,r.getIdch(),r.getDapAnSv(),r.getThuTuChon(),false);
+            luaChons = luaChonRepository.findAllByIdch(r.getIdch());
+            luaChons.sort((lc1,lc2)->(lc1.getThuTu() - lc2.getThuTu()));
+            ds.addCauHoiThi(new CauHoiThiDTO(cauHoi,luaChons,ctBaiThi));
         }
-        finally{
-            JDBCUtil.closeConnection(c);
-        }
+        ds.getList().sort((ds1,ds2)->(ds1.getCtBaiThi().getThuTuChon() - ds2.getCtBaiThi().getThuTuChon()));       
+        session.close();
         return ds;
     }
 }
